2f4f5a04a450f7a99de7861c6f5bad73e88d5fd3
AmoDinho <u14284783@tuks.co.za>
2022-07-21 00:23:51 +0200

    everything is scaffolded

--- nizhny-frontend/stacks/MyStack.ts
+++ nizhny-frontend/stacks/MyStack.ts
@@ -1,15 +1,13 @@
 import { StackContext, Api, NextjsSite } from '@serverless-stack/resources';
 import { App } from 'aws-cdk-lib';
 
-export function MyStack({ stack }: StackContext) {
-
-  const site = new NextjsSite(stack,"nizhny-frontend",{
-    path:"frontend",{
-      environment:{
-        REGION: app.region
-      }
-    }
-  })
+export function MyStack({ stack, app }: StackContext) {
+  const site = new NextjsSite(stack, 'nizhny-frontend', {
+    path: 'frontend',
+    environment: {
+      REGION: app.region,
+    },
+  });
 
   stack.addOutputs({
     URL: site.url,
