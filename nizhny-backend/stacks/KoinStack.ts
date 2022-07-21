a1c4a6f6345a11b8cef7bb78d8d228df83f672e7
AmoDinho <u14284783@tuks.co.za>
2022-07-21 23:14:23 +0200

    import s3 into main stack

--- nizhny-backend/stacks/KoinStack.ts
+++ nizhny-backend/stacks/KoinStack.ts
@@ -1,8 +1,11 @@
 import { StackContext } from '@serverless-stack/resources';
 import KoinDynamoDBResources from './dynamoDB';
+import ResourceS3 from './s3';
+
 const KoinStack = ({ stack }: StackContext) => {
   return {
     KoinDynamoDBResources: KoinDynamoDBResources,
+    ResourceS3: ResourceS3(stack),
   };
 };
 export default KoinStack;
