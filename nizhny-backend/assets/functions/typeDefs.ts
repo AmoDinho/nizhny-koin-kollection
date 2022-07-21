f9f72e05a50e5c05be1ea846a00d7e496d25f3f7
AmoDinho <u14284783@tuks.co.za>
2022-07-21 22:46:09 +0200

    making coin type

--- nizhny-backend/assets/functions/typeDefs.ts
+++ nizhny-backend/assets/functions/typeDefs.ts
@@ -4,10 +4,21 @@ type Team {
   name:String!
   bio: String!
 }
+
+enum KoinType {
+  BRONZE
+  SILVER
+  GOLD
+  PLATINUM
+}
 type Koin {
   id: String!
   imgURL: String!
   team: Team!
+  koinName: String!
+  koinType: KoinType!
+  koinValue: Int!
+
 }
   type Query {
     hello: String
