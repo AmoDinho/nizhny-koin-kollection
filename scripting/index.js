9e8754225be7899c371e43ccc7dc7c509b0818a6
AmoDinho <u14284783@tuks.co.za>
2023-08-14 21:43:41 +0200

    able to get all the players

--- scripting/index.js
+++ scripting/index.js
@@ -3,9 +3,19 @@ const path = require('path');
 
 const localFolderPath = path.join(__dirname, 'images');
 
-const uploadPictures = () => {
+const uploadPictures = async () => {
   try {
+    const files = await fs.promises.readdir(localFolderPath);
+
+    files.map(async (file) => {
+      const filePath = path.join(localFolderPath, file);
+      const fileData = await fs.promises.readFile(filePath);
+
+      console.log('file', file, fileData);
+    });
   } catch (e) {
     console.error('eeeror', e.message);
   }
 };
+
+uploadPictures();
