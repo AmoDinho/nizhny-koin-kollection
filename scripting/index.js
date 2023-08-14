1227d55b05ed3d22f3e3cec6175231532b7b6b00
AmoDinho <u14284783@tuks.co.za>
2023-08-14 21:39:44 +0200

    creating base

---
+++ scripting/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const localFolderPath = path.join(__dirname, 'images');
+
+const uploadPictures = () => {
+  try {
+  } catch (e) {
+    console.error('eeeror', e.message);
+  }
+};
