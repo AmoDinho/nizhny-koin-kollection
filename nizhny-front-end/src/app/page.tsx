0ea000450fd2433cb19305bfe1ac0378b74f0516
AmoDinho <u14284783@tuks.co.za>
2024-02-10 17:51:08 +0200

    added images

--- nizhny-front-end/src/app/page.tsx
+++ nizhny-front-end/src/app/page.tsx
@@ -1,16 +1,19 @@
 import ParentImage from '@/components/image/parentImage';
 import HeaderImage from '@/static/images/nizhny_banner.svg';
+import BottomImage from '@/static/images/bottom_illustrations.svg';
+
 import { TypographyWrapper } from '@/components/typography';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <TypographyWrapper
         WrapperTypes="HeadingOne"
-        additonalClassNames="text-primary-red-dark"
+        additonalClassNames="text-primary-red-dark mb-5"
       >
         Nizhny Koin Kollection
       </TypographyWrapper>
       <ParentImage imagePath={HeaderImage} />
+      <ParentImage imagePath={BottomImage} />
     </main>
   );
 }
