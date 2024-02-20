1c1e2eb145201288d60cc91fde9e5ecc480e29ce
AmoDinho <u14284783@tuks.co.za>
2024-02-20 08:51:31 +0200

    created middleware

---
+++ nizhny-front-end/middleware.ts
@@ -0,0 +1,25 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+
+import type { NextRequest } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user && req.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/home', req.url));
+  }
+  if (!user && req.nextUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ['/', '/account'],
+};
