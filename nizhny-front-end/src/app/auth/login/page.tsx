c09f0cd98d7dd2d24830549ab643c2f9061573a5
AmoDinho <u14284783@tuks.co.za>
2024-02-23 18:05:02 +0200

    fixing type issues

--- nizhny-front-end/src/app/auth/login/page.tsx
+++ nizhny-front-end/src/app/auth/login/page.tsx
@@ -1,5 +1,40 @@
+'use client';
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import type { Database } from '@/types/supabase';
+
+type TFormData = {
+  email: string;
+  password: string;
+};
+
+type TFormPayload = {
+  key: string;
+  value: string;
+};
+
+type IKeyFieldsType = 'email' | 'password';
 export default function Login() {
-  <main>
-    <p>Login</p>
-  </main>;
+  const [formState, setFormState] = useState<TFormData>({
+    email: '',
+    password: '',
+  });
+  const router = useRouter();
+  const supabase = createClientComponentClient<Database>();
+
+  const handleFormChange = ({ key, value }: TFormPayload) => {
+    let formStateClone: TFormData = { ...formState };
+    formStateClone[key as IKeyFieldsType] = value;
+
+    setFormState(formStateClone);
+  };
+
+  return (
+    <main>
+      <p>Login</p>
+    </main>
+  );
 }
