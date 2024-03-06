7addb0e5d00b67f3df664e5a4718e726ef365ec3
AmoDinho <u14284783@tuks.co.za>
2024-03-06 12:55:43 +0200

    done with setting up form

--- nizhny-front-end/src/app/auth/login/page.tsx
+++ nizhny-front-end/src/app/auth/login/page.tsx
@@ -3,9 +3,19 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import type { Database } from '@/types/supabase';
-
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 type TFormData = {
   email: string;
   password: string;
@@ -23,6 +33,7 @@ export default function Login() {
     password: '',
   });
   const router = useRouter();
+  const form = useForm();
   const supabase = createClientComponentClient<Database>();
 
   const handleFormChange = ({ key, value }: TFormPayload) => {
@@ -32,29 +43,48 @@ export default function Login() {
     setFormState(formStateClone);
   };
 
-  const handleSignIn = async (): Promise<void> => {
-    await supabase.auth.signInWithPassword({
-      email: formState.email,
-      password: formState.password,
-    });
-    router.refresh();
+  const handleSignIn = async (values): Promise<void> => {
+    console.log('values', values);
+    // await supabase.auth.signInWithPassword({
+    //   email: formState.email,
+    //   password: formState.password,
+    // });
+    // router.refresh();
   };
 
   return (
-    <main>
-      <input
-        name="email"
-        onChange={(e) =>
-          handleFormChange({ key: 'email', value: e.currentTarget.value })
-        }
-      />
-      <input
-        name="password"
-        onChange={(e) =>
-          handleFormChange({ key: 'password', value: e.currentTarget.value })
-        }
-      />
-      <button onClick={handleSignIn}>Sign In</button>
-    </main>
+    <div className="flex justify-center">
+      <h1>Log In</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSignIn)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </form>
+        <Button onClick={() => router.push('/auth/login')}>Log In</Button>
+      </Form>
+      {/* <input
+          name="email"
+          onChange={(e) =>
+            handleFormChange({ key: 'email', value: e.currentTarget.value })
+          }
+        />
+        <input
+          name="password"
+          onChange={(e) =>
+            handleFormChange({ key: 'password', value: e.currentTarget.value })
+          }
+        />
+        <button onClick={handleSignIn}>Sign In</button> */}
+    </div>
   );
 }
