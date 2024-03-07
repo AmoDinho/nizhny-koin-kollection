a76575b6748c0d544eb3b52f457cb6fda0d64de3
AmoDinho <u14284783@tuks.co.za>
2024-03-07 12:17:56 +0200

    busy setting up search params

---
+++ nizhny-front-end/src/app/team/create/page.jsx
@@ -0,0 +1,9 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
+
+export default function CreateTeam() {
+    const searchParams = useSearchParams()
+    const currentTabState = searchParams.get('tabState')
+  console.log(`currentTabState: ${}`)
+  return <></>;
+}
---
+++ nizhny-front-end/src/app/team/create/tabs/pageOne.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { TypographyWrapper } from '@/components/typography';
+// import { useSearchParams } from 'next/navigation';
+export default function PageOne() {
+  return (
+    <>
+      <TypographyWrapper WrapperTypes="HeadingOne">
+        What is your Team name?
+      </TypographyWrapper>
+    </>
+  );
+}
