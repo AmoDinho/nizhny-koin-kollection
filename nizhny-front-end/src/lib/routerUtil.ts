421058f7c50dee022724a5127bb1504b5b3ae6a6
AmoDinho <u14284783@tuks.co.za>
2024-03-07 17:31:33 +0200

    pause

---
+++ nizhny-front-end/src/lib/routerUtil.ts
@@ -0,0 +1,10 @@
+/*
+
+@param routeTarget: string
+@param paramsConstructor: func
+return function
+*/
+
+import { useRouter } from 'next/router';
+
+export const RouterUtil = (routeTarget: string, paramsConstructor) => {};
