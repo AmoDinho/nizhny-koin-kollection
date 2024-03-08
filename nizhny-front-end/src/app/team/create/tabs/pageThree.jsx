998c9de97b4191946fbd3c55e18c2b874d589a4e
AmoDinho <u14284783@tuks.co.za>
2024-03-08 15:12:29 +0200

    setting up empty state

--- nizhny-front-end/src/app/team/create/tabs/pageThree.jsx
+++ nizhny-front-end/src/app/team/create/tabs/pageThree.jsx
@@ -4,7 +4,12 @@ import { TypographyWrapper } from '@/components/typography';
 import { Button } from '@/components/ui/button';
 import useRouterUtil from '@/lib/useRouterUtil';
 import { useSearchParams } from 'next/navigation';
-export default function PageOne() {
+import { useRecoilValue } from 'recoil';
+import { createTeamState } from '@/state/atom';
+import ParentImage from '@/components/image/parentImage';
+import CoinHolder from '@/static/images/coin.svg';
+export default function PageThree() {
+  const team = useRecoilValue(createTeamState);
   const searchParams = useSearchParams();
   const { handleRouteChange } = useRouterUtil();
 
@@ -19,9 +24,15 @@ export default function PageOne() {
 
       <div>
         <div className="bg-white rounded-lg h-10 w-10">
-          <span></span>
-          <span></span>
-          <span></span>
+          {team.map((players, playerIndex) => (
+            <>
+              {players.Insert.teamID === null ? (
+                <ParentImage imagePath={CoinHolder} />
+              ) : (
+                <p>Here is your player</p>
+              )}
+            </>
+          ))}
         </div>
       </div>
 
