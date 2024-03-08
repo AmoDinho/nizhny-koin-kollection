b73b53664f96b737b4648a7363cad2de16f7aacf
AmoDinho <u14284783@tuks.co.za>
2024-03-08 12:44:08 +0200

    created types & arrays

--- nizhny-front-end/src/app/team/create/tabs/pageTwo.tsx
+++ nizhny-front-end/src/app/team/create/tabs/pageTwo.tsx
@@ -1,11 +1,31 @@
 'use client';
-
+import { CheckCircleIcon } from '@heroicons/react/16/solid';
 import { TypographyWrapper } from '@/components/typography';
-// import { useSearchParams } from 'next/navigation';
+import { IFeatureArray } from '@/types/types';
 export default function PageTwo() {
+  const featuresArray: IFeatureArray = [
+    {
+      id: '1',
+      label: '$300 for players',
+      icon: <CheckCircleIcon />,
+    },
+    {
+      id: '2',
+      label: '5 players in the squad',
+      icon: <CheckCircleIcon />,
+    },
+    {
+      id: '3',
+      label: 'Access to main league',
+      icon: <CheckCircleIcon />,
+    },
+  ];
   return (
     <>
-      <TypographyWrapper WrapperTypes="HeadingOne">
+      <TypographyWrapper
+        WrapperTypes="HeadingOne"
+        additonalClassNames="text-red-600"
+      >
         What you get
       </TypographyWrapper>
     </>
---
+++ nizhny-front-end/src/types/types.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+type IFeatures = {
+  id: string;
+  label: string;
+  icon: React.JSX.Element;
+};
+
+export type IFeatureArray = IFeatures[];
