2628e51eea3e683ed8a02ea5f33cd6483bed9f78
AmoDinho <u14284783@tuks.co.za>
2024-03-11 17:20:38 +0200

    trying to make a dialog

---
+++ nizhny-front-end/src/components/ui/player/playerModal.tsx
@@ -0,0 +1,16 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+
+  export default function PlayerModal(){
+
+    return (
+        <Dialog
+    )
+  }
