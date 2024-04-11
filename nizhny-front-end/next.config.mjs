05771f0c72f27ff871d4925403df12cc57869d82
AmoDinho <u14284783@tuks.co.za>
2024-04-11 17:05:49 +0200

    modal is rendering the players

--- nizhny-front-end/next.config.mjs
+++ nizhny-front-end/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'zpcbncxtprtxcantyxey.supabase.co',
+        pathname: '/storage/v1/object/public/nizhny-koin-ref/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
