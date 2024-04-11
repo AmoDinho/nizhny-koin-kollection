43f3b6d5937f8126b91e321b66689648855ecce6
AmoDinho <u14284783@tuks.co.za>
2024-04-11 17:17:36 +0200

    created getPlayerCount function

--- nizhny-front-end/src/services/players.ts
+++ nizhny-front-end/src/services/players.ts
@@ -4,4 +4,11 @@ const getPaginatedPlayers = async () => {
   let { data: Players, error } = await useSupabase.from('Players').select('*');
   return { Players, error };
 };
-export { getPaginatedPlayers };
+
+const getPlayerCount = async () => {
+  let { data: Count, error: CountError } = await useSupabase
+    .from('Players')
+    .select('*');
+  return { Count, CountError };
+};
+export { getPaginatedPlayers, getPlayerCount };
