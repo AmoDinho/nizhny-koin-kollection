ba8d3dbdf7572267f9df37fb6a473c18df1cc86d
AmoDinho <u14284783@tuks.co.za>
2024-04-11 17:11:19 +0200

    added default width

--- nizhny-front-end/src/components/image/parentImage.tsx
+++ nizhny-front-end/src/components/image/parentImage.tsx
@@ -3,20 +3,24 @@ interface ImageProps {
   imagePath: string;
   additionalClassNames?: string;
   onClick?: () => void;
+  width?: Number;
+  height?: Number;
 }
 
 const ParentImage = ({
   imagePath,
   additionalClassNames,
   onClick,
+  width,
+  height,
 }: ImageProps): React.JSX.Element => (
   <Image
     onClick={onClick}
     src={imagePath}
     alt="something went wrong"
     className={additionalClassNames}
-    width={500}
-    height={500}
+    width={width ? width : 100}
+    height={height ? height : 100}
   />
 );
 
