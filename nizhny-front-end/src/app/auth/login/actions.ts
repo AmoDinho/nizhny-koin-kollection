e41b2186e282bfc69983157c243d452d6f348088
AmoDinho <u14284783@tuks.co.za>
2024-05-02 14:55:14 +0200

    added ssr client

--- nizhny-front-end/src/app/auth/login/actions.ts
+++ nizhny-front-end/src/app/auth/login/actions.ts
@@ -1,3 +1,12 @@
 export async function login(formData) {
   console.log('formData', formData);
+
+  //sign user in
+  //add the cookies to local storage
+
+  //set the user in the recoil store
+
+  //redirect user
+
+  //return errors
 }
--- nizhny-front-end/src/lib/useSupabase.ts
+++ nizhny-front-end/src/lib/useSupabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 import type { Database } from '@/types/supabase';
 
-const useSupabase = createClient<Database>(
+const useSupabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 );
