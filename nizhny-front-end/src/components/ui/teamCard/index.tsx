2b36c9fb167ac1ee9ba29b4c54d8438d5f84797e
AmoDinho <u14284783@tuks.co.za>
2024-05-06 10:50:36 +0200

    creating team card

---
+++ nizhny-front-end/src/components/ui/teamCard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card, CardContent } from '../card';
+const TeamCard = (): React.JSX.Element => {
+  return (
+    <Card>
+      <CardContent className="flex items-center gap-4"></CardContent>
+    </Card>
+  );
+};
